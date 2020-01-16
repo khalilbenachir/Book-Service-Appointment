4c0d3620183c311ebb5bdf8d54f9fbbbca37c362
BENACHIR Khalil <benachir1996khalil@gmail.com>
2020-01-16 13:50:28 +0100

    finished chatbot

---
+++ config/api.js
@@ -0,0 +1,29 @@
+var apiai = require('apiai');
+
+// read the api.ai docs : https://api.ai/docs/
+
+//Enter your API Key
+var app = apiai('6043da33b26f467ab2b3a86905695fb1');
+
+// Function which returns speech from api.ai
+var getRes = function(query) {
+  var request = app.textRequest(query, {
+      sessionId: '<unique session id>'
+  });
+const responseFromAPI = new Promise(
+        function (resolve, reject) {
+request.on('error', function(error) {
+    reject(error);
+});
+request.on('response', function(response) {
+  resolve(response.result.fulfillment.speech);
+});
+});
+request.end();
+return responseFromAPI;
+};
+
+// test the command :
+//getRes('hello').then(function(res){console.log(res)});
+
+module.exports = {getRes}
